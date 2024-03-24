cf5bf4033941428b705f040ede71cb4fac9d54a9
Uldis <uldisgabers@gmail.com>
2024-03-24 14:38:00 +0200

    Initial Commit

---
+++ GIPHYSearchApp/GIPHYSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIPHYSearchAppApp.swift
+//  GIPHYSearchApp
+//
+//  Created by Uldis on 24/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct GIPHYSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
